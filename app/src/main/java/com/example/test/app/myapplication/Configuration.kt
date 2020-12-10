ef9d666857e2f7a944fa879c3446109c6654fe8f
Bhargav Mogra <bhargav.mogra@agoda.com>
2020-12-10 19:16:47 +0700

    Validate and extract types in Processor
    * Look for KSDeclarations annotated with Mapper annotation
    * Validate the functions in the KSDeclarations
    * Extract MapFunctionDeclaration from it
    * Delegate to code gen to generate for each MapFunctionDeclaration.

--- app/src/main/java/com/example/test/app/myapplication/Configuration.kt
+++ app/src/main/java/com/example/test/app/myapplication/Configuration.kt
@@ -1,5 +1,8 @@
 package com.example.test.app.myapplication
 
+import com.bhargavms.mappergen.annotations.Mapper
+
+@Mapper
 interface Configuration {
     fun map(value: Network.HotelDetailsPOI): Domain.HotelDetailPOI
 }
--- mappergen/src/main/kotlin/com/bhargavms/mappergen/compiler/MapperGenProcessor.kt
+++ mappergen/src/main/kotlin/com/bhargavms/mappergen/compiler/MapperGenProcessor.kt
@@ -1,19 +1,25 @@
 package com.bhargavms.mappergen.compiler
 
 import com.bhargavms.mappergen.annotations.Mapper
+import com.bhargavms.mappergen.code.generator.MapFunctionDeclaration
+import com.bhargavms.mappergen.code.generator.generate
 import com.bhargavms.mappergen.compiler.errors.BadAnnotationTargetException
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 
 class MapperGenProcessor : SymbolProcessor {
     private lateinit var realProcessor: RealProcessor
     override fun finish() {
     }
 
-    override fun init(options: Map<String, String>, kotlinVersion: KotlinVersion, codeGenerator: CodeGenerator, logger: KSPLogger) {
+    override fun init(
+        options: Map<String, String>, kotlinVersion: KotlinVersion, codeGenerator: CodeGenerator,
+        logger: KSPLogger
+    ) {
         realProcessor = RealProcessor(codeGenerator)
     }
 
@@ -23,17 +29,45 @@ class MapperGenProcessor : SymbolProcessor {
     }
 
     private class RealProcessor(
-            val codeGenerator: CodeGenerator
+        val codeGenerator: CodeGenerator
     ) {
         fun process(resolver: Resolver) {
             resolver.getSymbolsWithAnnotation(Mapper::class.qualifiedName!!)
                     .map {
                         (it as? KSClassDeclaration)
-                                ?: throw BadAnnotationTargetException(Mapper::class, "interface/class")
+                        ?: throw BadAnnotationTargetException(Mapper::class, "interface/class")
                     }
-                    .map {
-//                        generate()
+                    .forEach { declaration ->
+                        generateMappersForInterface(declaration) {
+                            it.generate(
+                                resolver, codeGenerator,
+                                declaration.packageName.asString()
+                            )
+                        }
                     }
         }
     }
+
+    companion object {
+        private inline fun generateMappersForInterface(
+            declaration: KSClassDeclaration,
+            generator: (MapFunctionDeclaration) -> Unit
+        ) {
+            declaration.getAllFunctions()
+                    .map(KSFunctionDeclaration::extract)
+                    .forEach(generator)
+        }
+    }
+}
+
+private fun KSFunctionDeclaration.extract(): MapFunctionDeclaration {
+    val params = parameters
+    val returnType = returnType
+
+    if (params.size != 1 || returnType == null) {
+        throw IllegalStateException(
+            "functions need to have 1 parameter and 1 return type"
+        )
+    }
+    return MapFunctionDeclaration(params[0].type!!.resolve(), returnType.resolve())
 }
